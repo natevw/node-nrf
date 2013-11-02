499b300b7e4d9d35e841b8e7633d4b618538bf70
Nathan Vander Wilt <nate@calftrail.com>
2013-11-02 16:59:57 -0700

    add little test notes

--- gpio.js
+++ gpio.js
@@ -1,5 +1,14 @@
 var fs = require('fs');
 
+
+/*
+var pin1 = require("./gpio").connect(17),
+    pin2 = require("./gpio").connect(22);
+pin2.mode('in');
+pin1.mode('high'); pin2.value();
+pin1.mode('low'); pin2.value();
+*/
+
 exports.connect = function (pin) {        // TODO: sync up compat, split out
     pin = +pin;
     
