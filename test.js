4c4f6965ae59434244f10a5bee2a02d1eac3905f
Nathan Vander Wilt <nate@calftrail.com>
2013-11-08 17:27:13 -0800

    fix typo

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var NRF24 = require("./index");
 
 var nrf24 = NRF24.connect("/dev/spidev0.0", 24);
 
-nrf.setStates({RF_CH:42,PWR_UP:true}, function (e,d) {
+nrf24.setStates({RF_CH:42,PWR_UP:true}, function (e) {
     if (e) console.error("Couldn't set states:",e);
     else nrf24.getStates(['EN_CRC','RX_ADDR_P0','RF_CH','PWR_UP','RX_P_NO'], function (e,d) {
         if (e) console.error("Couldn't get states:",e);
