2db3e9197c98b350a5e97820d4d8a0b91b191d69
Nathan Vander Wilt <nate@calftrail.com>
2013-11-29 11:20:15 -0800

    printDetails after test script setup is complete

--- test.js
+++ test.js
@@ -2,15 +2,8 @@
 
 var NRF24 = require("./index"),
     spiDev = "/dev/spidev0.0",
-    cePin = 24, irqPin = 25;            //var ce = require("./gpio").connect(cePin)
-
-var nrf = NRF24.connect(spiDev, cePin, irqPin);
-nrf.printDetails();
-
-
-
-
-var pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2];
+    cePin = 24, irqPin = 25,            //var ce = require("./gpio").connect(cePin)
+    pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2];
 
 
 var stream = require('stream'),
@@ -24,15 +17,16 @@ TimeStream.prototype._read = function () {
     this.push(new Date().toISOString());
 };
 
-setTimeout(function () {
-    nrf._debug = true;
-    nrf.channel(0x4c).dataRate('1Mbps').crcBytes(2).begin(function () {
-        var tx = nrf.openPipe('tx', pipes[0], {autoAck:false});
-        tx.on('ready', function () {
-            //(new TimeStream).pipe(tx);
-            setInterval(function () {
-                tx.write('zyxa');
-            }, 1e3);
-        });
+
+
+var nrf = NRF24.connect(spiDev, cePin, irqPin);
+nrf.channel(0x4c).dataRate('1Mbps').crcBytes(2).begin(function () {
+    var tx = nrf.openPipe('tx', pipes[0], {autoAck:false});
+    tx.on('ready', function () {
+        nrf.printDetails();
+        //(new TimeStream).pipe(tx);
+        setInterval(function () {
+            tx.write('zyxa');
+        }, 1e3);
     });
-}, 1e3);      // HACK: wait for printDetails to finish
+});
