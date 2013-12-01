5164949cd2d6f46df83943818fd61cfaf3898a11
Nathan Vander Wilt <nate@calftrail.com>
2013-11-30 22:53:51 -0800

    slow down RX_P_NO check a bit, fix up to apply whether IRQ triggered or not as even when reading the state seems to get mismatched

--- index.js
+++ index.js
@@ -365,7 +365,11 @@ exports.connect = function (spi,ce,irq) {
     nrf._checkStatus = function (irq) {
         nrf.getStates(['RX_P_NO','TX_DS','MAX_RT','RX_DR'], function (e,d) {
             if (e) nrf.emit('error', e);
-            else if (irq && d.RX_DR && d.RX_P_NO === 0x07) nrf._checkStatus(irq);       // HACK: poll until things shape up!
+            else if (d.RX_DR && d.RX_P_NO === 0x07) process.nextTick(function () {
+                // HACK: chip seems to assert RX_DR a while before setting RX_P_NO, so poll if necessary
+                // TODO: is there one of the settle T values that applies here? (doesn't seem so in quick testing)
+                nrf._checkStatus(irq);
+            });
             else if (irq || d.RX_P_NO !== 0x07 || d.TX_DS || d.MAX_RT) nrf.emit('interrupt', d);
         });
     };
