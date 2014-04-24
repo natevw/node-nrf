dde9c5f18a729be39009b4bc2113eb2cf38df375
Nathan Vander Wilt <nate@calftrail.com>
2014-04-23 21:31:34 -0700

    allow override of pin info via CLI args

--- test-details.js
+++ test-details.js
@@ -1,6 +1,9 @@
 var NRF24 = require("./index"),
-    spiDev = "/dev/spidev0.1",
-    cePin = 24, irqPin = 25;
+    spiDev = process.argv[2] || "/dev/spidev0.0",
+    cePin = +process.argv[3] || 24,
+    irqPin = +process.argv[4] || 25;
 
-var nrf = NRF24.connect(spiDev, cePin, irqPin);
+var nrf = (irqPin > 0) ?
+    NRF24.connect(spiDev, cePin, irqPin) :
+    NRF24.connect(spiDev, cePin);
 nrf.printDetails();
